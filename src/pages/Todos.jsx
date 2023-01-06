1d21f08368bc3a7b614e8697a325f8cda29bc1aa
Nurlan Ibragimov <nibragimov@1cb.kz>
2023-01-06 14:49:35 +0600

    fix: add useeffect deps

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,35 +1,50 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {observer} from "mobx-react-lite";
 
 import Todo from "../components/common/Todo/Todo";
 import Filter from "../components/common/Filter/Filter";
 import Sidebar from "../components/common/Sidebar/Sidebar";
 import BaseInput from "../components/base/BaseInput/BaseInput";
-import todo from "../store/todo";
-import filter from "../store/filter";
+import todoStore from "../store/todo";
+import filterStore from "../store/filter";
 
 const Todos = observer(() => {
+    const todos = todoStore.todos;
+    const filter = filterStore.filter;
     const [shown, setShown] = useState(false);
     const [value, setValue] = useState("");
-    const filteredTodos = useMemo(() => {
-        switch (filter.filter) {
+    const [filteredTodos, setFilteredTodos] = useState([]);
+    // const filteredTodos = useMemo(() => {
+    //     switch (filterStore.filter) {
+    //         case "completed":
+    //             return todoStore.todos.filter(todo => todo.completed);
+    //         case "uncompleted":
+    //             return todoStore.todos.filter(todo => !todo.completed);
+    //         default:
+    //             return todoStore.todos;
+    //     }
+    // }, [todoStore.todos, filterStore.filter]);
+    useEffect(() => {
+        switch (filterStore.filter) {
             case "completed":
-                return todo.todos.filter(todo => todo.completed);
+                setFilteredTodos(todoStore.todos.filter(todo => todo.completed));
+                break;
             case "uncompleted":
-                return todo.todos.filter(todo => !todo.completed);
+                setFilteredTodos(todoStore.todos.filter(todo => !todo.completed));
+                break;
             default:
-                return todo.todos;
+                setFilteredTodos(todoStore.todos);
         }
-    }, [filter.filter, todo.todos]);
+    }, [filter, todos]);
     const fetchTodos = async () => {
-        await todo.fetchTodo();
+        await todoStore.fetchTodo();
     }
     const onChange = (e) => {
         setValue(e.target.value);
     }
     const addTodo = () => {
-        todo.add({
-            id: todo.todos.length + 1,
+        todoStore.add({
+            id: todoStore.todos.length + 1,
             title: value,
             completed: false
         });
@@ -42,9 +57,9 @@ const Todos = observer(() => {
             <div className={"py-8"}>
                 <Filter/>
                 <div className={"flex justify-center"}>
-                    <button disabled={todo.loading} className={"!bg-gray-100 !text-dark max-w-xs mx-auto mb-8"}
+                    <button disabled={todoStore.loading} className={"!bg-gray-100 !text-dark max-w-xs mx-auto mb-8"}
                             onClick={fetchTodos}>
-                        {!todo.error ? "Get todos" : todo.error}
+                        {!todoStore.error ? "Get todos" : todoStore.error}
                     </button>
                 </div>
                 <div className={"flex flex-col gap-2 max-w-md mx-auto"}>
@@ -59,7 +74,7 @@ const Todos = observer(() => {
             </div>
             <Sidebar shown={shown} setState={setShown}>
                 <BaseInput name={"todo"} value={value} placeholder={"type here..."} onChange={onChange}/>
-                <button disabled={todo.loading} onClick={addTodo}>
+                <button disabled={todoStore.loading} onClick={addTodo}>
                     Add
                 </button>
             </Sidebar>
