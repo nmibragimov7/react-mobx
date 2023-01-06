55384691eda417a8c3d5bfaaea4be56091dc5f54
Nurlan Ibragimov <nibragimov@1cb.kz>
2023-01-06 14:50:25 +0600

    +fix

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {observer} from "mobx-react-lite";
 
 import Todo from "../components/common/Todo/Todo";
